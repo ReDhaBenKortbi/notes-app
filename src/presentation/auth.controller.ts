9444a9d13575eac0e28876f3325bf3793800b302
ReDhaBenKortbi <ridhabenkortbi@gmail.com>
2024-12-23 19:04:32 +0100

    Refactore responses and add req.body type

--- src/presentation/auth.controller.ts
+++ src/presentation/auth.controller.ts
@@ -4,94 +4,85 @@ import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { generateToken } from "../utils/generateToken";
-
+interface LoginData {
+  username: string;
+  password: string;
+}
 class AuthController {
   private userRepo;
   constructor(userRepo: UserRepository) {
     this.userRepo = userRepo;
   }
-  login = asyncHandler(async (req: Request, res: Response) => {
-    //@ts-ignore
-    const loginData: { username: string; password: string } = req.body;
+  login = asyncHandler(
+    async (req: Request<{}, {}, LoginData>, res: Response) => {
+      //@ts-ignore
+      const loginData: { username: string; password: string } = req.body;
 
-    if (!loginData.username || !loginData.password) {
-      res.status(400).json({
-        message: "all fields are required",
-      });
-      return;
-    }
+      if (!loginData.username || !loginData.password) {
+        res.status(400).json({ message: "All fields are required." });
+        return;
+      }
 
-    const user = await this.userRepo.getUserByUsername(loginData.username);
+      const user = await this.userRepo.getUserByUsername(loginData.username);
 
-    if (user === null) {
-      res.status(401).json({
-        message: "user doesn't exist",
-      });
-      return;
-    } else if (!user.isActive) {
-      res.status(401).json({
-        message: "user is not active",
-      });
-      return;
-    } else {
+      if (!user) {
+        res.status(404).json({ message: "User not found." });
+        return;
+      }
+
+      if (!user.isActive) {
+        res.status(403).json({ message: "User is not active." });
+        return;
+      }
+
       const match = await bcrypt.compare(loginData.password, user.password);
 
       if (!match) {
-        res.status(401).json({
-          message: "username or password is wrong",
-        });
-      } else {
-        const accessToken = generateToken("access", {
-          userInfo: {
-            username: user.username,
-            roles: user.roles,
-          },
-        });
-        const refreshToken = generateToken("refresh", {
-          username: user.username,
-        });
-
-        res
-          .cookie("jwt", refreshToken, {
-            httpOnly: true, //accessible only by the server
-            secure: true, //for https
-            sameSite: "none",
-            maxAge: 1 * 24 * 60 * 60 * 1000, //1d
-          })
-          .json({
-            accessToken,
-          });
+        res.status(401).json({ message: "Invalid credentials." });
+        return;
       }
+
+      const accessToken = generateToken("access", {
+        userInfo: {
+          username: user.username,
+          roles: user.roles,
+        },
+      });
+      const refreshToken = generateToken("refresh", {
+        username: user.username,
+      });
+
+      res
+        .cookie("jwt", refreshToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+          maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+        })
+        .json({ accessToken });
     }
-  });
+  );
+
   refresh = asyncHandler(async (req: Request, res: Response): Promise<void> => {
     const cookies = req.cookies;
 
-    if (!cookies.jwt) {
-      res.status(401).json({ message: "No refresh token" });
+    if (!cookies?.jwt) {
+      res.status(401).json({ message: "No refresh token provided." });
       return;
     }
 
     const refreshToken = cookies.jwt;
 
     try {
-      const decoded = await new Promise((resolve, reject) => {
-        jwt.verify(
-          refreshToken,
-          process.env.REFRESH_TOKEN_SECRET || "",
-          (err: any, decoded: any) => {
-            if (err) reject(err);
-            resolve(decoded);
-          }
-        );
-      });
+      const decoded = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET || ""
+      ) as { username: string };
 
-      const user = await this.userRepo.getUserByUsername(
-        (decoded as any).username
-      );
+      const user = await this.userRepo.getUserByUsername(decoded.username);
 
       if (!user) {
-        res.status(404).json({ message: "User not found" });
+        res.status(404).json({ message: "User not found." });
         return;
       }
 
@@ -104,7 +95,10 @@ class AuthController {
 
       res.json({ accessToken });
     } catch (error) {
-      res.status(403).json({ message: "Invalid token" });
+      res.status(403).json({
+        status: 403,
+        message: "Token is invalid or expired",
+      });
     }
   });
 
@@ -112,7 +106,7 @@ class AuthController {
     const cookies = req.cookies;
 
     if (!cookies?.jwt) {
-      res.status(204).json();
+      res.status(204).send(); // No content
       return;
     }
 
@@ -122,7 +116,7 @@ class AuthController {
       secure: true,
     });
 
-    res.json({ message: "Cookie was cleared" });
+    res.status(200).json({ message: "Cookie was cleared." });
   });
 }
 export default AuthController;
